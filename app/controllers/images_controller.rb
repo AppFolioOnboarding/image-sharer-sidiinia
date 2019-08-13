48ae2adcec7e10ccba7d3ea78806d240ab447bbf
Sydney Ma <siyi.ma@appfolio.com>
2019-08-13 13:59:30 -0700

    sm - Save data in the controller
    Signed-off-by: Sydney Ma <siyi.ma@appfolio.com>

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,6 +3,14 @@ class ImagesController < ApplicationController
   end
 
   def create
-    render plain: params[:image].inspect
+    @image = Image.new(image_params)
+
+    @image.save
+    redirect_to @image
+  end
+
+  private
+  def image_params
+    params.require(:image).permit(:title, :text)
   end
 end
