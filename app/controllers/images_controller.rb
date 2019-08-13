314a12eaee7079a14ae001659e7d230aa9bc2c91
Sydney Ma <siyi.ma@appfolio.com>
2019-08-13 16:05:50 -0700

    sm - Add validations to check if user is proving valid URL
    Signed-off-by: Sydney Ma <siyi.ma@appfolio.com>

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,19 +2,23 @@ class ImagesController < ApplicationController
   def index
     @images = Image.all
   end
-  
+
   def show
     @image = Image.find(params[:id])
   end
 
   def new
+    @image = Image.new
   end
 
   def create
     @image = Image.new(image_params)
 
-    @image.save
-    redirect_to @image
+    if @image.save
+      redirect_to @image
+    else
+      render 'new'
+    end
   end
 
   private
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,18 @@
 class Image < ApplicationRecord
+  validates :title, presence: true
+  validates :text, presence: true
+  validate :validates_url
+
+  private
+
+  def validates_url
+    url = resolve_url(text)
+    errors.add(:text, 'Invalid URL') unless url.present?
+  end
+
+  def resolve_url(img_url)
+    url = URI.parse(img_url)
+    return url if url.host
+  rescue URI::Error
+  end
 end
