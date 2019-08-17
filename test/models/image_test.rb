ba6410de3ea631169c943fc0039ad78cfdab020b
Sydney Ma <siyi.ma@appfolio.com>
2019-08-16 17:09:11 -0700

    sm - Add model unit tests
    Signed-off-by: Sydney Ma <siyi.ma@appfolio.com>

--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class ImageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_image__valid_if_resource_is_image
+    image = Image.new(title: 'image_example', text: 'https://data.whicdn.com/images/62682708/original.jpg')
+    assert_predicate image, :valid?
+  end
+
+  def test_image__invalid_if_title_is_blank
+    image = Image.new(title: '', text: 'https://data.whicdn.com/images/62682708/original.jpg')
+    refute_predicate image, :valid?
+    assert_equal "can't be blank", image.errors[:title].first
+  end
+
+  def test_image__invalid_if_text_is_blank
+    image = Image.new(title: 'sydney', text: '')
+    refute_predicate image, :valid?
+    assert_equal "can't be blank", image.errors[:text].first
+
+  end
+
+  def test_image__invalid_if_url_is_invalid
+    image = Image.new(title: 'sydney', text: 'www.notValidUrl.com')
+    refute_predicate image, :valid?
+    assert_equal "Invalid URL", image.errors[:text].first
+  end
 end
