506d8819c7b4cb039a2b9768e90fc2c42e9df75f
Sydney Ma <siyi.ma@appfolio.com>
2019-08-19 09:42:57 -0700

    sm - Add controller tests
    Signed-off-by: Sydney Ma <siyi.ma@appfolio.com>

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class ImagesControllerTest < ActionDispatch::IntegrationTest
+
   # test "the truth" do
   #   assert true
   # end
+  setup do
+    @image = Image.create!(title: 'sydney', text: 'http://blog.bongiovi.tw/wp-content/uploads/2015/03/03.jpg')
+  end
+
+  def test_index
+    get images_path
+
+    assert_response :ok
+    assert_select 'h1', 'Listing images'
+
+  end
+
+  def test_new
+    get new_image_path
+
+    assert_response :ok
+    assert_select 'form'
+  end
+
+  def test_show
+    get image_path(@image.id)
+
+    assert_response :ok
+    assert_select '#name', 'sydney'
+  end
 end
